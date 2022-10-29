51dd7c923a1d4e9c8c5548d4cd1b441217b7a72c
Yeimipao <yeimipao1@gmail.com>
2022-10-29 20:07:54 -0300

    se realizo las rutas

--- back/app.js
+++ back/app.js
@@ -1,4 +1,11 @@
 const express=require("express");
 const app = express();
 
+app.use(express.json());
+
+//importar rutas
+const productos=require("./routes/products")
+
+app.use('/api',productos) // sujeto cambios
+
 module.exports=app

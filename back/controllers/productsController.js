793daa627d18af922e925ab0099530ba610063b1
Yeimipao <yeimipao1@gmail.com>
2022-11-06 20:26:55 -0300

    empezar agregar productos

--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -1,6 +1,20 @@
+const { json } = require("express");
+const producto=require("../models/productos")
+
+//Ver la lista de productos
 exports.getProducts=(req,res,next) => {
     res.status(200).json({
         success: true,
         message: "En esta ruta ud va a poder ver todos los productos"
+    })
+}
+
+//crear nuevo producto
+exports.newProduct=async(req,res,next)=>{
+    const product=await producto.create(req.body);
+
+    res.status(201).json({
+        success:true,
+        product
     })
 }
--- back/routes/products.js
+++ back/routes/products.js
@@ -1,8 +1,9 @@
 const express=require("express")
 const router=express.Router();
 
-const {getProducts} = require("../controllers/productsController")
+const {getProducts, newProduct} = require("../controllers/productsController")
 
 router.route('/productos').get(getProducts)
+router.route('/producto/nuevo').post(newProduct);//establecemos la ruta
 
 module.exports=router;
